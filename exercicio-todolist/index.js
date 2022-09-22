101ec87934ae8b034288543272ea8e63aae5d744
Smz1n <samuel.gomesv2@gmail.com>
2022-09-22 11:39:18 -0300

    atualizando

---
+++ exercicio-todolist/index.js
@@ -0,0 +1,13 @@
+function addTarefa() {
+    event.preventDefault();
+
+    let titulo = document.getElementById('input_nova_tarefa').value;
+
+    document.getElementById('input_nova_tarefa').value = '';
+
+    document.getElementById('tarefas').innerHTML += `
+    <div>
+        <input type="checkbox"> ${titulo}
+    </div>
+    `;
+}
