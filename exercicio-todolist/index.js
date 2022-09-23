b17dbf184110a2ef8fc9fd9db27e617dc4c40013
Smz1n <samuel.gomesv2@gmail.com>
2022-09-23 11:39:36 -0300

    atualizando

--- exercicio-todolist/index.js
+++ exercicio-todolist/index.js
@@ -1,13 +1,45 @@
+let lista_tarefas = [];
+
+function atualizarQuantidade() {
+    document.getElementById('numeros').innerHTML = lista_tarefas.length;
+}
+
+function listarTarefas() {
+    let conteudo = lista_tarefas.sort().map(function (tarefa) {
+        return `
+    <div>
+        <input type="checkbox"> ${tarefa}
+    </div>
+    `;
+});
+
+document.getElementById('tarefas').innerHTML = conteudo.join('');
+
+}
+
 function addTarefa() {
     event.preventDefault();
 
     let titulo = document.getElementById('input_nova_tarefa').value;
 
+    if (titulo.trim() === ''){
+        alert ('Tarefa invalida');
+        return;
+    }
+
+    if(lista_tarefas.includes(titulo)) {
+        alert ('Tarefa ja existe');
+        return;
+    }
+
+    if
+
+
+    lista_tarefas.push(titulo);
+
     document.getElementById('input_nova_tarefa').value = '';
 
-    document.getElementById('tarefas').innerHTML += `
-    <div>
-        <input type="checkbox"> ${titulo}
-    </div>
-    `;
+
+    atualizarQuantidade();
+    listarTarefas();
 }
